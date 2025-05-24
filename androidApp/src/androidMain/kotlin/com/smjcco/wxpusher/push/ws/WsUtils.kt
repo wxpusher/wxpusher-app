7b6eba8590923e3954f814a6671c0b007ae1cbad
zjiecode <zjiecode@gmail.com>
2025-05-24 14:24:02 +0800

    处理弹窗顺序问题

--- androidApp/src/androidMain/kotlin/com/smjcco/wxpusher/push/ws/WsUtils.kt
+++ androidApp/src/androidMain/kotlin/com/smjcco/wxpusher/push/ws/WsUtils.kt
@@ -3,6 +3,7 @@ package com.smjcco.wxpusher.push.ws
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
+import com.smjcco.wxpusher.dialog.DialogManager
 import com.smjcco.wxpusher.page.CheckActivity
 import com.smjcco.wxpusher.utils.ApplicationUtils
 import com.smjcco.wxpusher.utils.SaveUtils
@@ -17,7 +18,7 @@ object WsUtils {
         if (SaveUtils.getByKey("noteKeepAlive") == "1") {
             return
         }
-        AlertDialog.Builder(activity)
+        val dialog = AlertDialog.Builder(activity)
             .setTitle("保活提示")
             .setMessage("由于Android的系统限制，应用在后台会被限制运行，导致收不到消息，请打开后台限制。")
             .setPositiveButton(
@@ -32,6 +33,7 @@ object WsUtils {
                 dialog?.dismiss()
                 SaveUtils.setKeyValue("noteKeepAlive", "1")
             }
-            .create().show()
+            .create()
+        DialogManager.show(activity, dialog)
     }
 }
