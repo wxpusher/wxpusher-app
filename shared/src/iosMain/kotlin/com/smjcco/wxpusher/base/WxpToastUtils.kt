27c5df9c693a84a4e7e0ac1b18eb1454ce201d10
zjiecode <zjiecode@gmail.com>
2025-07-25 11:31:27 +0800

    修改toast的位置

--- shared/src/iosMain/kotlin/com/smjcco/wxpusher/base/WxpToastUtils.kt
+++ shared/src/iosMain/kotlin/com/smjcco/wxpusher/base/WxpToastUtils.kt
@@ -1,10 +1,12 @@
 package com.smjcco.wxpusher.base
 
 import cocoapods.Toaster.Toast
+import cocoapods.Toaster.ToastView
 import kotlinx.cinterop.ExperimentalForeignApi
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun ExpWxpToastUtils_showToast(msg: String){
+    ToastView.appearance().setBottomOffsetPortrait(450.0)
     Toast(text = msg, delay = 0.0, duration = 2.0).show()
 //    val toast = Toast()
 //    toast.setText(msg)
