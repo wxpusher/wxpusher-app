7c27a8ce5672ad1d9878e955ca4d5d5bdd0c6a73
zjiecode <zjiecode@gmail.com>
2025-10-12 18:25:18 +0800

    支持魅族推送

--- androidAppV2/build.gradle.kts
+++ androidAppV2/build.gradle.kts
@@ -64,8 +64,8 @@ android {
         applicationId = "com.smjcco.wxpusher"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 10500
-        versionName = "1.5.0"
+        versionCode = 10501
+        versionName = "1.5.1"
         //指定产物名称
         setProperty("archivesBaseName", "wxpusher-app-v$versionName")
         
