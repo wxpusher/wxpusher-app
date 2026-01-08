4e92c07e7021bd8ecbfef6e0ceb63194837d15b8
zjiecode <zjiecode@gmail.com>
2026-01-09 00:06:17 +0800

    1.6.1

--- androidAppV2/build.gradle.kts
+++ androidAppV2/build.gradle.kts
@@ -64,8 +64,8 @@ android {
         applicationId = "com.smjcco.wxpusher"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 10600
-        versionName = "1.6.0"
+        versionCode = 10601
+        versionName = "1.6.1"
         //指定产物名称
         setProperty("archivesBaseName", "wxpusher-app-v$versionName")
         
