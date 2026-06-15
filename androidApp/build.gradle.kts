54dc46e7c5a72b045abacb3e92a89e234b49cf8a
zjiecode <zjiecode@gmail.com>
2026-06-15 21:33:22 +0800

    Android && iOS 1.7.50

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -67,8 +67,8 @@ android {
         applicationId = "com.smjcco.wxpusher"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 10730
-        versionName = "1.7.30"
+        versionCode = 10750
+        versionName = "1.7.50"
         //指定产物名称
         setProperty("archivesBaseName", "wxpusher-app-v$versionName")
 
