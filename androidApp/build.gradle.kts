9a46c32cde516222a46af1ae71c12b2d007c3637
zjiecode <zjiecode@gmail.com>
2026-08-30 20:03:36 +0800

    Android 1.8.15

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -67,8 +67,8 @@ android {
         applicationId = "com.smjcco.wxpusher"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 10811
-        versionName = "1.8.11"
+        versionCode = 10815
+        versionName = "1.8.15"
         //指定产物名称
         setProperty("archivesBaseName", "wxpusher-app-v$versionName")
 
